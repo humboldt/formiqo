0aef4871f308c59076ded3c2dc0ba39c55a20d57
kirqe <ikll7@yahoo.com>
2017-08-25 04:02:28 +0300

    random fx

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: ENV["host"], port: ENV["port"] }
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+      api_key: ENV['mailgun_api_key'],
+      domain: ENV['mailgun_domain']
+  }
+
+  config.middleware.use(Rack::Tracker) do
+    handler :google_analytics, { tracker: ENV["google"]}
+  end
 end
