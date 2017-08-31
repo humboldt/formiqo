7aed739e613f9e1d26e9cb3842b144c8d671b511
kirqe <ikll7@yahoo.com>
2017-08-31 15:31:08 +0300

    fts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV["host"], port: ENV["port"] }
+  config.action_mailer.default_url_options = { host: ENV["host"] }
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
