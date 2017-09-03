7ac991ab90d01d50dcf507aea0f539b4389c1b17
kirqe <ikll7@yahoo.com>
2017-09-03 04:10:23 +0300

    adjustments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create(email: ENV["user_email"], password: ENV["user_password"], admin: true)
-Plan.create(name: "Personal", description: "10 mailboxes", cost: 6.99, n_mailboxes: 10)
+Plan.create(name: "Personal", description: "10 mailboxes", cost: 4.99, n_mailboxes: 10)
 
 
 # Sample data
